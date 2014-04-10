70ee882bcc286bb98b02de0cc13f7dc1b95c14f6
straktor <a.boulay56@gmail.com>
2014-04-10 16:53:23 -0400

    cannot read .wav

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,49 @@
 (function($){
-
+  
+  var goat1 = new Audio('http://adstorage.jamba.net/storage/view/320/0/go/Goat.mp3');
+  
+  
+  $(document).keydown(function(e){
+    switch(e.keyCode){
+      case 49:
+//          goat1.play();      
+        break;
+      case 50:
+//          goat2.play();
+        break;
+      case 51:
+//          goat3.play();  
+        break;
+      case 52:
+//          goat4.play();  
+        break;
+      case 53:
+//          goat5.play();  
+        break;
+      case 54:
+//          goat6.play();
+        break;
+      case 55:
+//          goat7.play();
+        break;
+      case 56:
+//          goat8.play();
+        break;
+      case 57:
+//          goat9.play();
+        break;
+      case 48:
+//          goat10.play();
+        break;
+      default:
+//          goat1.play();
+        break;
+    }
+  });
+  
 })(jQuery);
+
+
+// 48 = 0
+// 49 = 1
+// 57 = 9
