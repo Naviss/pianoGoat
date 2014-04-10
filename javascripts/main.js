936721a427b5f649d9b4578212c043fed90bed65
straktor <a.boulay56@gmail.com>
2014-04-10 17:37:53 -0400

    goat close eyes

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,49 +14,118 @@
   $(document).keydown(function(e){
     switch(e.keyCode){
       case 49:
-          goat1.play();      
+        goat1.play();   
+        document.getElementById("goat1").src = "images/comicGoatclose.png";
         break;
       case 50:
-          goat2.play();
+        goat2.play();
+        document.getElementById("goat2").src = "images/comicGoatclose.png";
         break;
       case 51:
-          goat3.play();  
+        goat3.play();  
+        document.getElementById("goat3").src = "images/comicGoatclose.png";
         break;
       case 52:
-          goat4.play();  
+        goat4.play();  
+        document.getElementById("goat4").src = "images/comicGoatclose.png";
         break;
       case 53:
-          goat5.play();  
+        goat5.play();
+        document.getElementById("goat5").src = "images/comicGoatclose.png";
         break;
       case 54:
-          goat6.play();
+        goat6.play();
+        document.getElementById("goat6").src = "images/comicGoatclose.png";
         break;
       case 55:
-          goat7.play();
+        goat7.play();
+        document.getElementById("goat7").src = "images/comicGoatclose.png";
         break;
       case 56:
-          goat8.play();
+        goat8.play();
+        document.getElementById("goat8").src = "images/comicGoatclose.png";
         break;
       case 57:
-          goat9.play();
+        goat9.play();
+        document.getElementById("goat9").src = "images/comicGoatclose.png";
         break;
       case 48:
-          goat10.play();
+        goat10.play();
+        document.getElementById("goat10").src = "images/comicGoatclose.png";
         break;
       default:
-          goat1.play();
-          goat2.play();
-          goat3.play();
-          goat4.play();
-          goat5.play();
-          goat6.play();
-          goat7.play();
-          goat8.play();
-          goat9.play();
-          goat10.play();
+        goat1.play();
+        goat2.play();
+        goat3.play();
+        goat4.play();
+        goat5.play();
+        goat6.play();
+        goat7.play();
+        goat8.play();
+        goat9.play();
+        goat10.play();
+        document.getElementById("goat1").src = "images/comicGoatclose.png";
+        document.getElementById("goat2").src = "images/comicGoatclose.png";
+        document.getElementById("goat3").src = "images/comicGoatclose.png";
+        document.getElementById("goat4").src = "images/comicGoatclose.png";
+        document.getElementById("goat5").src = "images/comicGoatclose.png";
+        document.getElementById("goat6").src = "images/comicGoatclose.png";
+        document.getElementById("goat7").src = "images/comicGoatclose.png";
+        document.getElementById("goat8").src = "images/comicGoatclose.png";
+        document.getElementById("goat9").src = "images/comicGoatclose.png";
+        document.getElementById("goat10").src = "images/comicGoatclose.png";
         break;
     }
   });
+  
+  $(document).keyup(function(e){
+    switch(e.keyCode){
+      case 49:
+          document.getElementById("goat1").src = "images/comicGoat.png";
+        break;
+      case 50:
+        document.getElementById("goat2").src = "images/comicGoat.png";
+        break;
+      case 51:
+        document.getElementById("goat3").src = "images/comicGoat.png";
+        break;
+      case 52:
+        document.getElementById("goat4").src = "images/comicGoat.png";
+        break;
+      case 53: 
+        document.getElementById("goat5").src = "images/comicGoat.png";
+        break;
+      case 54:
+        document.getElementById("goat6").src = "images/comicGoat.png";
+        break;
+      case 55:
+        document.getElementById("goat7").src = "images/comicGoat.png";
+        break;
+      case 56:
+        document.getElementById("goat8").src = "images/comicGoat.png";
+        break;
+      case 57:
+        document.getElementById("goat9").src = "images/comicGoat.png";
+        break;
+      case 48:
+        document.getElementById("goat10").src = "images/comicGoat.png";
+        break;
+      default:
+        document.getElementById("goat1").src = "images/comicGoat.png";
+        document.getElementById("goat2").src = "images/comicGoat.png";
+        document.getElementById("goat3").src = "images/comicGoat.png";
+        document.getElementById("goat4").src = "images/comicGoat.png";
+        document.getElementById("goat5").src = "images/comicGoat.png";
+        document.getElementById("goat6").src = "images/comicGoat.png";
+        document.getElementById("goat7").src = "images/comicGoat.png";
+        document.getElementById("goat8").src = "images/comicGoat.png";
+        document.getElementById("goat9").src = "images/comicGoat.png";
+        document.getElementById("goat10").src = "images/comicGoat.png";
+        break;
+    }
+  });
+  
+  
   
 })(jQuery);
 
